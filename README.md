2d91116c9c794aaea9f0ba757bdb2b75e00f0c69
Marvin Richner <marvin.richner@stud.fhgr.ch>
2021-11-05 19:40:28 +0100

    Set Up Flask, Templating, Basic Structure and an empty bootstrap website

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sticky Notes (App)
+
+## Image Licenses
+
+### Background Images
+All used background images are provided free of charge from Unsplash.com
+- ```static/media/bg01.jpg```: Birger Strahl, Lüneburger Heide, Deutschland
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/notes")
+def notes():
+    return render_template('notes.html')
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
+
+@app.route('/media/<path:path>')
+def send_media(path):
+    return send_from_directory('static/media', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
