7f2a110b5b4e070e31d75656e7dfe63a0b513eac
Marvin Richner <marvin.richner@stud.fhgr.ch>
2021-11-26 17:48:29 +0100

    edited database

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ from flask import Flask, render_template, send_from_directory, request, redirect
 
 DATABASE = 'notes.db'
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -27,3 +39,9 @@ def send_media(path):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
