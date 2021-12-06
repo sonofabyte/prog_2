1ed8b926fa5c235c6d9c6d65bae43c5f858406c5
Marvin Richner <marvin.richner@stud.fhgr.ch>
2021-12-06 13:07:33 +0100

    cards can now be edited, added insert and update SQL queries

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sqlite3
+from typing import Text
 from flask import Flask, render_template, send_from_directory, request, redirect, url_for, g, session
+import datetime
 
 DATABASE = 'notes.db'
 
@@ -15,6 +17,12 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
+def write_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    get_db().commit()
+    cur.close()
+
 app = Flask(__name__)
 app.secret_key = 'any random string'
 
@@ -26,10 +34,54 @@ def hello_world():
 def notes():
     if request.method == 'GET':
         notes = query_db("select * from notes")
-        print(notes)
-        return render_template('notes.html')
+
+        cards = []
+
+        for note in notes:
+            card = dict()
+            card["ID"] = note[0]
+            card["Title"] = note[1]
+            card["Content"] = note[2]
+            note_date = datetime.datetime.strptime(note[3], '%d/%m/%y %H:%M:%S')
+            now_date = datetime.datetime.now()
+            date_delta = now_date - note_date
+            card["Modified"] = date_delta.days
+            cards.append(card)
+
+        return render_template('notes.html', cards = cards)
     if request.method == 'POST':
         return render_template('notes.html')
+
+@app.route('/api/modNote', methods = ['POST'])
+def modNote():
+    if not ("process" in request.form):
+        content = request.form.get("content", Text)
+        title = request.form.get("title", Text)
+        post_id = request.form.get("itemID", int)
+
+        time_now_string = datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S')
+        print(time_now_string)
+
+        if int(request.form["itemID"]) == 0:
+            # make new note in DB
+            write_db('insert into notes (title, content, date) values (?, ?, ?)', [title, content, time_now_string])
+
+        else:
+            write_db('update notes set title = ?, content = ?, date = ? where ID = ?', [title, content, time_now_string, post_id])
+
+        return redirect(url_for('notes'))
+
+    elif request.form["process"] == "get":
+        note = query_db('select * from notes where ID = ?', [request.form["item_id"]], one = True)
+        resp = dict()
+        resp["ID"] = note[0]
+        resp["title"] = note[1]
+        resp["content"] = note[2]
+        return resp
+
+@app.route('/api/delNote', methods = ['POST'])
+def delNote():
+    print("delNote ", request.form["item_id"])
 
 @app.route('/js/<path:path>')
 def send_js(path):
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,48 @@
 //alert("im alive");
+
+function delNote(id) {
+  $.ajax({
+    url: "/api/modNote",
+    method: "POST",
+    data: { item_id: id, process: "get"},
+  }).done(function(response) {
+    console.log(response);
+
+  }).fail(function( jqXHR, textStatus ) {
+    alert("fail");
+  });
+}
+
+var exampleModal = document.getElementById('newNote')
+
+//configure modal content on open
+exampleModal.addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget;
+    var itemN = 0;
+    var title_edit = exampleModal.querySelector('#titleedit');
+    var text_edit = exampleModal.querySelector('#textedit');
+    var idField = exampleModal.querySelector('#idField');
+
+    if (button.getAttribute('data-bs-edit') == "true"){
+        itemN = button.getAttribute('data-bs-item');
+        $.ajax({
+            url: "/api/modNote",
+            method: "POST",
+            data: { item_id: itemN, process: "get"},
+          }).done(function(response) {
+            console.log(response);
+            title_edit.value = response.title;
+            text_edit.value = response.content;
+    
+          }).fail(function( jqXHR, textStatus ) {
+            alert("fail");
+          });
+    }
+    else{
+      title_edit.value = "";
+      text_edit.value = "";
+    }
+
+    idField.value = itemN;
+
+})
